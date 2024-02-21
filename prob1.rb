6d4c654b355d50bb52f75389fc1420c3e2355f1f
Dalton Mann <daltonjamann@gmail.com>
2024-02-20 20:37:07 -0600

    Completed problem 1

---
+++ prob1.rb
@@ -0,0 +1,19 @@
+class Laptop
+  def initialize(brand, model)
+    @brand = brand
+    @model = model
+  end
+
+  def brand
+    @brand
+  end
+
+  def model
+    @model
+  end
+end
+
+laptop = Laptop.new("Apple", "Mac")
+
+puts "Brand: #{laptop.brand}"
+puts "Model: #{laptop.model}"
